3a429fad260adf6b882b3954f097b77e7144185d
w-haibara <hwhaibarawataru@gmail.com>
2018-10-24 01:12:53 +0900

    生成したQRコードをqrcode/に貯めるようにした

--- src/qrMaker.py
+++ src/qrMaker.py
@@ -1,14 +1,16 @@
 import sys
 import qrcode
+import subprocess
 
 argv = sys.argv;
 argc = len(argv)
 
 if argc == 2:
-    img = qrcode.make(argv[1])
-    img.show()
-    img.save('qrcode' + argv[1] + '.png')
-    sys.exit(0)
+	img = qrcode.make(argv[1])
+	img.show()
+	name ='qrcode' + argv[1] + '.png'
+	img.save(name)
+	subprocess.call(["mv", name, "./qrCode"])
+	sys.exit(0)
 else:
     sys.exit(1)
-
