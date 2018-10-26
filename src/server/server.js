96778d872a0d7525da5a85d352a7bf65ff87a20d
w-haibara <hwhaibarawataru@gmail.com>
2018-10-27 06:00:15 +0900

    軽微な修正

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'));
+const rl=require("readline").createInterface(process.stdin,process.stdout);
+
+var str = "hello";
+
+(async function(){
+    str=await new Promise(res=>rl.once("line",res));
+    console.log("Input : " + str);
+})();
+
+
+app.get('/', (req, res) => res.send(str));
 app.listen(3000, () => console.log('Listening on port 3000'));
